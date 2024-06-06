d86b4f409d885705379436626fb6fafe8166afc0
Arthur Miguel <Amsbsimoes@gmail.com>
2024-06-06 15:43:39 -0300

    adicionando estrutura

---
+++ academia/src/test/java/com/academiaGenerica/academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academiaGenerica.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
