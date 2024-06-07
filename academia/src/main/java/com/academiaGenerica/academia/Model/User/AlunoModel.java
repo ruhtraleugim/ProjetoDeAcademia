c3d87d71fda7f52249ade200a93cbf49235584de
Arthur Miguel <Amsbsimoes@gmail.com>
2024-06-07 19:16:19 -0300

    adição de 2 novos arquivos

---
+++ academia/src/main/java/com/academiaGenerica/academia/Model/User/AlunoModel.java
@@ -0,0 +1,30 @@
+package com.academiaGenerica.academia.Model.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Aluno")
+@Table(name = "Aluno")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "idAluno")
+public class AlunoModel {
+    private String nome;
+    private String cpf;
+    private String cel;
+    private String email;
+    private String aulas;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idAluno;
+}
---
+++ academia/src/main/java/com/academiaGenerica/academia/Model/User/FuncionarioModel.java
@@ -0,0 +1,29 @@
+package com.academiaGenerica.academia.Model.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Funcionario")
+@Table(name = "Funcionario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "idFuncionario")
+public class FuncionarioModel {
+    private String name;
+    private String CPF;
+    private String tel;
+    private String email;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFuncionario;
+}
