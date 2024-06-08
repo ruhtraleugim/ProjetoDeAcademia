e7a22bb52fd455f1dc1bae87fe783186d0de3040
gustavo <luiz.carvalho7110@sounidesc.com.br>
2024-06-08 19:35:51 -0300

    atulização !

--- academia/src/main/java/com/academiaGenerica/academia/Model/Transações/Transactions.java
+++ academia/src/main/java/com/academiaGenerica/academia/Model/Transações/Transactions.java
@@ -1,5 +1,44 @@
 package com.academiaGenerica.academia.Model.Transações;
 
 public class Transactions {
+    private String nome ;
+     private int cpf;
+   private  String planodeacademia;
+
+   public Transactions(String nome, int cpf, String planodeacademia) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.planodeacademia = planodeacademia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public String getPlanodeacademia() {
+        return planodeacademia;
+    }
     
+    public void setPlanodeacademia(String planodeacademia) {
+        this.planodeacademia = planodeacademia;
+    }
+
+
+
+
+
+    {
+        
+    }
 }
